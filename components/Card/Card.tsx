60df8e37672ec13e6eb23fb27128a0833396ed67
Andrew Cacayuran <andrewcacayuran@Andrews-MacBook-Pro.local>
2021-09-19 16:25:03 -0700

    test

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,19 +1,19 @@
-import { FC } from "react";
+import React, { FC } from "react";
 
 interface Props {
-    title?: string;
-    id?: number;
-    img?: string;
-    subtext?: string;
+  title?: string;
+  id?: number;
+  img?: string;
+  subtext?: string;
 }
 
 const Card: FC<Props> = ({ title, id, img, subtext }) => {
-    return (
-        <div>
-            <h1>{title}</h1>
-            <p>{subtext}</p>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>{subtext}</p>
+    </div>
+  );
+};
 
 export default Card;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,31 @@
 import axios from "axios";
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import { useCallback, useEffect, useState } from "react";
-import Card from "../components/Card/Card";
+import React, { useCallback, useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
 import { MovieDetail } from "../utils/MovieDetails";
 
 const Home: NextPage = () => {
   const [movies, setMovies] = useState<MovieDetail[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(true)
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   const fetchAPI = useCallback(async () => {
-    const { data } = await axios.get('https://mcuapi.herokuapp.com/api/v1/movies')
-    setMovies(data.data)
-    setIsLoading(false)
-  }, [])
-  console.log(movies)
+    const { data } = await axios.get(
+      "https://mcuapi.herokuapp.com/api/v1/movies"
+    );
+    setMovies(data.data);
+    setIsLoading(false);
+  }, []);
+  console.log(movies);
   useEffect(() => {
-    fetchAPI()
-  }, [])
+    fetchAPI();
+  }, []);
 
   return (
     <div className={styles.container}>
-      {movies.map(movie => {
+      {movies.map((movie) => {
         <div key={movie.id}>
           <p>{movie.title}</p>
-        </div>
+        </div>;
       })}
       {/* <pre>{JSON.stringify(movies, null, 2)}</pre> */}
     </div>
