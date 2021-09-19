b9ed09524cb14b560d6f70844f75cabaddfc9a25
Andrew Cacayuran <andrewcacayuran@Andrews-MacBook-Pro.local>
2021-09-19 15:16:16 -0700

    why no work

---
+++ components/Card/Card.tsx
@@ -0,0 +1,18 @@
+import { FC } from "react";
+
+interface Props {
+    title?: string;
+    id?: number;
+    img?: string;
+    subtext?: string;
+}
+
+const Card: FC<Props> = ({ title, id, img, subtext }) => {
+    return (
+        <div>
+            <h1>{title}</h1>
+        </div>
+    )
+}
+
+export default Card;
