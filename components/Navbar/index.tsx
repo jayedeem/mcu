2781a76ad1e8b9921de0702e095d883a496ffe0c
Drew C <jayedeem@gmail.com>
2022-03-01 20:53:49 -0800

    stuff

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -2,22 +2,22 @@ import { NextComponentType } from 'next';
 import styles from './navbar.module.scss';
 
 export const NavBar = (): JSX.Element => {
-    return (
-        <div className={styles.navbar__container}>
-            <div className={styles.navbar__container_wrapper}>
-                <div className={styles.navbar__container_logo}>
-                    <h2>Marvel MCU APP</h2>
-                </div>
-                <div className={styles.navbar__container_menu}>
-                    <div className={styles.navbar__container_menu_items}>
-                        <ul>
-                            <li>Home</li>
-                            <li>About</li>
-                            <li>Contact</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className={styles.navbar__container}>
+      <div className={styles.navbar__container_wrapper}>
+        <div className={styles.navbar__container_logo}>
+          <h2>Marvel MCU APP</h2>
         </div>
-    );
+        <div className={styles.navbar__container_menu}>
+          <div className={styles.navbar__container_menu_items}>
+            <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
