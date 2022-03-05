229b315ee441b827cf155c19780e15b1e5d4f177
Drew C <jayedeem@gmail.com>
2022-03-05 10:58:21 -0800

    mobile nav, must yarn install for icons

--- components/MobileNav/index.tsx
+++ components/MobileNav/index.tsx
@@ -1,40 +1,40 @@
-import { FC, useState, MouseEventHandler } from "react";
+import { FC, useState, MouseEventHandler } from 'react';
 import s from './mobilenav.module.scss';
-import { GiHamburgerMenu } from 'react-icons/gi'
-import { AiOutlineCloseCircle } from 'react-icons/ai'
-
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { AiOutlineCloseCircle } from 'react-icons/ai';
 
 const MobilenavOverlay = ({ isToggle }: any) => {
-    return (
-        <div className={isToggle ? s.mobileNav__overlay : s.mobileNav__overlayHidden}>
-            <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
-            </ul>
-        </div>
-    )
-}
-
+  return (
+    <div className={isToggle ? s.mobileNav__overlay : s.mobileNav__overlayHidden}>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+};
 
 export const MobileNav: FC = () => {
-    const [isToggle, setIsToggle] = useState<boolean>(false)
+  const [isToggle, setIsToggle] = useState<boolean>(false);
 
-    const toggleOverlay = () => {
-        setIsToggle(!isToggle)
-    }
+  const toggleOverlay = () => {
+    setIsToggle(!isToggle);
+  };
 
-    return (
-        <div className={s.mobileNav}>
-            <div className={s.mobileNav__hamburger} >
-                {
-                    isToggle ? <AiOutlineCloseCircle onClick={() => toggleOverlay()} /> : <GiHamburgerMenu onClick={() => toggleOverlay()} />
-                }
-            </div>
+  return (
+    <div className={s.mobileNav}>
+      <div className={s.mobileNav__hamburger}>
+        {isToggle ? (
+          <AiOutlineCloseCircle onClick={() => toggleOverlay()} />
+        ) : (
+          <GiHamburgerMenu onClick={() => toggleOverlay()} />
+        )}
+      </div>
 
-            <>
-                <MobilenavOverlay isToggle={isToggle} />
-            </>
-        </div>
-    )
-}
+      <>
+        <MobilenavOverlay isToggle={isToggle} />
+      </>
+    </div>
+  );
+};
